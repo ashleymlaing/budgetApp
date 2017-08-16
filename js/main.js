f4066e3d0395dd44f2e83a719801c0b5749fb9e4
Ashley Marie Laing <ashleym.laing@gmail.com>
2017-08-16 17:20:13 -0400

    the values are stroing but budget4month is coming as NAN, need to fix that

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
+  let cashInPocket;
+  let budget4month = parseFloat($('.budget').val());
+  let spentMoney = 0;
+  let remainingBudget = budget4month - spentMoney;
+
   $('.startingApp').on('click',function(){
-    var cashInPocket = $('.start').val();
-    const budget4month = $('.budget').val();
+    budget4month = parseFloat($('.budget').val());
+    cashInPocket = $('.start').val();
+    remainingBudget = budget4month - spentMoney;
+    console.log(budget4month);
+    console.log(spentMoney);
+
   });
+  $('.remainding-panel').html("<span>" + remainingBudget +" remaining</span>");
+  console.log(budget4month);
+  console.log(spentMoney);
 });
