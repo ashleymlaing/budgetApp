c908d5379f454cc67e9aa9273783aeeddedba6d8
resilientcoders6 <emilio.a.murillo@gmail.com>
2017-08-16 17:19:30 -0400

    adding the list function which is very messy for now

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     var cashInPocket = $('.start').val();
     const budget4month = $('.budget').val();
   });
+  // When you click on button, values will be added to the list
+  $(".add").on("click", function() {
+    var value = $("input").val();
+    var type = $("#type").val();
+    $("ul").append("<li>"+value+"</li>");
+    $("ul").append("<li>"+type+"</li>");
+
+    $("input").val("");
+  });
 });
